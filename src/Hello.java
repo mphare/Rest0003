89c06c79ba29e4b2d0735e78ec0a3641c6349f14
Michael Hare <mhare@zixcorp.com>
2016-06-02 13:22:51 -0500

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,26 @@
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import java.io.IOException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Path;
+
+/**
+ * Created by mhare on 6/2/16.
+ */
+// The Java class will be hosted at the URI path "/helloworld"
+@Path("/helloworld")
+public class Hello {
+    // The Java method will process HTTP GET requests
+    @GET
+    // The Java method will produce content identified by the MIME Media type "text/plain"
+    @Produces("text/plain")
+    public String getClichedMessage() {
+        // Return some cliched textual content
+        return "Hello from REST";
+    }
+
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mhare
+  Date: 6/2/16
+  Time: 1:07 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Hi JSP</title>
+  </head>
+  <body>
+  Hello from JSP
+  </body>
+</html>
